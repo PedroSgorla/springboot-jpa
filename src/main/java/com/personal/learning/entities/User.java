3d1e007b815bd1eab2d3d1343d6a0970856647c7
Pedro Sgorla <pedro.oliveira@atlantecnologia.com.br>
2026-08-05 15:09:12 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/personal/learning/entities/User.java
+++ src/main/java/com/personal/learning/entities/User.java
@@ -1,11 +1,14 @@
 package com.personal.learning.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,19 +21,22 @@ public class User implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String emial;
+	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
 	}
 
-	public User(Long id, String name, String emial, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.emial = emial;
+		this.email = email;
 		this.phone = phone;
 		this.password = password;
 	}
@@ -51,12 +57,12 @@ public class User implements Serializable{
 		this.name = name;
 	}
 
-	public String getEmial() {
-		return emial;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setEmial(String emial) {
-		this.emial = emial;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getPhone() {
@@ -73,6 +79,11 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
